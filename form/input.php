4e79e9a334b8d7b2c311adbc2a51877e2bec9d99
Kuroflectr <ike.kuroflectr@gmail.com>
2023-05-19 17:14:54 +0900

    the first commit

---
+++ form/input.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head></head>
+<body>
+
+<form method="GET", action="input.php">
+name
+<input type="text" name='your_name'>
+
+
+<input type>
+</form>
+
+
+</body>
+</html>
+
