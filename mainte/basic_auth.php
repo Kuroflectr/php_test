4fba9e26564150ea49b8aa420a984a9e9060227c
Kuroflectr <ike.kuroflectr@gmail.com>
2023-05-23 15:47:52 +0900

    basic auth

---
+++ mainte/basic_auth.php
@@ -0,0 +1,10 @@
+<?php
+
+//パスワードを記録したファイルの場所
+echo __FILE__;
+// /Applications/MAMP/htdocs/php_test/mainte/test.php
+
+echo '<br>';
+//パスワード(暗号化)
+echo(password_hash('password123', PASSWORD_BCRYPT));
+// $2y$10$SXwlPwmcIbm5uKNtn65ad.denaT1p.axYht21HUhvpcpu3M3raE8m
