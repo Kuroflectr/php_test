d1b17003a14ef95e9935641353c11d67959c0ffe
Kuroflectr <ike.kuroflectr@gmail.com>
2023-05-24 12:45:37 +0900

    database connection (added options)

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -4,11 +4,20 @@
 const DB_HOST = 'mysql:host=127.0.0.1;dbname=udemy_php;charset=utf8';
 const DB_USER = 'root'; 
 const DB_PASSWORD = ''; 
+// $driver_options = [
+//     変更したい属性 => 値,
+//     変更したい属性 => 値,
+// ];
+$driver_options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, 
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+    PDO::ATTR_EMULATE_PREPARES => false, 
+];
 
 try{ 
-    $pdo = new PDO(DB_HOST,DB_USER,DB_PASSWORD); 
-    echo 'success'; 
-}    catch( PDEException $e){
-    echo 'warning: failed'.$e-> getMessage()."\n"; 
+    $pdo = new PDO(DB_HOST,DB_USER,DB_PASSWORD, $driver_options); 
+    echo '接続成功'; 
+} catch( PDEException $e){
+    echo '接続失敗'.$e-> getMessage()."\n"; 
     exit(); 
 }
