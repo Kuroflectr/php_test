81447ba28a290d1d5015974e4263304c65ce13e0
Kuroflectr <ike.kuroflectr@gmail.com>
2023-05-29 16:08:31 +0900

    composer init

---
+++ composertest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hayashi/composertest',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '04524313f966d9877a190016d3d14b221515622b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hayashi/composertest' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '04524313f966d9877a190016d3d14b221515622b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
